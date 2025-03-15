79f8f53deac8b7493638bc2df230d621a39227f4
SparrowBrain <qwxmail@gmail.com>
2025-03-15 21:30:53 +0200

    Release tools

---
+++ ReleaseTools.IntegrationTests/InstallerManifestYaml/PlayniteSdkVersionParserTests.cs
@@ -0,0 +1,23 @@
+﻿using ReleaseTools.InstallerManifestYaml;
+using Xunit;
+
+namespace ReleaseTools.IntegrationTests.InstallerManifestYaml
+{
+    public class PlayniteSdkVersionParserTests
+    {
+        private readonly string _projectFile = $@"InstallerManifestYaml\TestData\{Program.AddonName}.csproj";
+
+        [Fact]
+        public void GetVersion_ReturnsVersionFromProjectFile()
+        {
+            // Arrange
+            var sut = new PlayniteSdkVersionParser(_projectFile);
+
+            // Act
+            var result = sut.GetVersion();
+
+            // Assert
+            Assert.Equal("70.50.60", result);
+        }
+    }
+}
