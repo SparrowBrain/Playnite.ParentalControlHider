907ba69b5b8425e13b598370f495cd95516aac9f
SparrowBrain <qwxmail@gmail.com>
2025-03-18 22:13:49 +0200

    Add missing source files

---
+++ ParentalControlHider/Services/GamesWhitelist.cs
@@ -0,0 +1,13 @@
+﻿using ParentalControlHider.Settings;
+using Playnite.SDK.Models;
+
+namespace ParentalControlHider.Services
+{
+	public class GamesWhitelist : IGamesWhitelist
+	{
+		public bool IsOnWhitelist(Game game, ParentalControlHiderSettings settings)
+		{
+			return settings.WhitelistedGameIds?.Contains(game.Id) ?? false;
+		}
+	}
+}
---
+++ ParentalControlHider/Services/IGamesWhitelist.cs
@@ -0,0 +1,10 @@
+﻿using ParentalControlHider.Settings;
+using Playnite.SDK.Models;
+
+namespace ParentalControlHider.Services
+{
+	public interface IGamesWhitelist
+	{
+		bool IsOnWhitelist(Game game, ParentalControlHiderSettings settings);
+	}
+}
